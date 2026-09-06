906baba5a287aa4070de3d80ff155d939cfb362a
Lucas <luc.ramirezr@duocuc.cl>
2026-09-06 17:22:47 -0300

    arreglos index y administrador

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ const inventarioProductos = [
         categoria: "Juegos de Mesa",
         nombre: "Catan",
         precio: 29000,
+        stock : 3,
         enOferta: true,
         descuento: 30,
         imagen: "img/catan.jpg"
@@ -16,6 +17,7 @@ const inventarioProductos = [
         categoria: "Juegos de Mesa",
         nombre: "Carcassonne",
         precio: 24990,
+        stock : 2,
         enOferta: false,
         descuento: 0,
         imagen: "img/carcassonne.png"
@@ -25,6 +27,7 @@ const inventarioProductos = [
         categoria: "Accesorios",
         nombre: "Controlador Inalámbrico Xbox Series X",
         precio: 59990,
+        stock : 2,
         enOferta: true,
         descuento: 15,
         imagen: "img/control-xbox.webp"
@@ -34,6 +37,7 @@ const inventarioProductos = [
         categoria: "Accesorios",
         nombre: "Auriculares Gamer HyperX Cloud II",
         precio: 79990,
+        stock : 2,
         enOferta: false,
         descuento: 20,
         imagen: "img/audifonos-hyperx.png"
@@ -43,6 +47,7 @@ const inventarioProductos = [
         categoria: "Consolas/Computadores",
         nombre: "PlayStation5",
         precio: 549990,
+        stock : 4,
         enOferta: false,
         descuento: 0,
         imagen: "img/play5.png"
@@ -52,6 +57,7 @@ const inventarioProductos = [
         categoria: "Consolas/Computadores",
         nombre: "PC Gamer ASUS ROG Strix",
         precio: 1299990,
+        stock : 2,
         enOferta: true,
         descuento: 15,
         imagen: "img/asus-rog.png"
@@ -61,6 +67,7 @@ const inventarioProductos = [
         categoria: "Perifericos",
         nombre: "Silla Gamer Secretlab Titan",
         precio: 349990,
+        stock : 5,
         enOferta: false,
         descuento: 0,
         imagen: "img/silla-gamer.png"
@@ -70,6 +77,7 @@ const inventarioProductos = [
         categoria: "Perifericos",
         nombre: "Mouse Gamer Logitech G502 HERO",
         precio: 49990,
+        stock : 5,
         enOferta: true,
         descuento: 5,
         imagen: "img/g502.png"
@@ -79,6 +87,7 @@ const inventarioProductos = [
         categoria: "Perifericos",
         nombre: "Mousepad Razer Goliathus Extended Chroma",
         precio: 29990,
+        stock : 10,
         enOferta: false,
         descuento: 0,
         imagen: "img/mousepad.png"
@@ -88,6 +97,7 @@ const inventarioProductos = [
         categoria: "Vestuario",
         nombre: "Polera Gamer Personalizada 'Level-Up'",
         precio: 14990,
+        stock : 30,
         enOferta: false,
         descuento: 0,
         imagen: "img/polera.jpg"
@@ -100,7 +110,11 @@ if (!localStorage.getItem("productos")) {
 }
 
 // ==========================================
-// 2. FILTRADO: TOP 3 OFERTAS
+// FILTRAR MEJORES OFERTAS
+// ==========================================
+
+// ==========================================
+// FILTRAR MEJORES OFERTAS
 // ==========================================
 const catalogoCompleto = JSON.parse(localStorage.getItem("productos")) || [];
 
@@ -110,7 +124,7 @@ const ofertasEspeciales = catalogoCompleto
     .slice(0, 3);
 
 // ==========================================
-// 3. RENDERIZADO DE TARJETAS DE OFERTA
+// MOSTRAR PRODUCTOS DE OFERTAS ESPECIALES
 // ==========================================
 const contenedor = document.getElementById("contenedor-ofertas");
 
